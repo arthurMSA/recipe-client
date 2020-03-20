77818cce881914e7b80a730380292ada61d6f661
arthur9010 <arthur.alves@redfox.tech>
2020-03-20 01:03:28 -0300

    FEAT: create card to recipe

---
+++ components/card/RecipeCard.vue
@@ -0,0 +1,30 @@
+<template>
+	<v-card>
+		<v-card-title>
+			{{ title }}
+		</v-card-title>
+		<v-card-text>
+			{{ content }}
+		</v-card-text>
+	</v-card>
+</template>
+<script>
+export default {
+	props: {
+		title: {
+			type: String,
+			required: false,
+			default: "Sem título"
+		},
+		content: {
+			type: String,
+			required: true
+		}
+	}
+}
+</script>
+<style scoped>
+.card-recipe {
+	border: 1px solid rgb(172, 172, 172);
+}
+</style>
