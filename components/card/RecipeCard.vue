0437e3741d48e50cb332b998f79c1f148b602ae4
arthur9010 <arthur.alves@redfox.tech>
2020-03-22 00:56:18 -0300

    FEAT: create crud

--- components/card/RecipeCard.vue
+++ components/card/RecipeCard.vue
@@ -1,8 +1,19 @@
 <template>
-	<v-card>
-		<v-card-title>
-			{{ title }}
+	<v-card @click="$emit('click', $event)" class="card-recipe" mix-height="200px">
+		<v-card-title class="font-weight-bold  text-uppercase d-flex justify-space-between">
+			<div class="align-end">
+				{{ title }}
+			</div>
+			<div class="btn-icon">
+				<v-btn class="btn-delete" icon @click="$emit('delete')">
+					<v-icon>delete_outline</v-icon>
+				</v-btn>
+				<v-btn class="btn-edit" icon @click="$emit('edit')">
+					<v-icon>edit</v-icon>
+				</v-btn>
+			</div>
 		</v-card-title>
+		<div class="divider mx-auto"></div>
 		<v-card-text>
 			{{ content }}
 		</v-card-text>
@@ -25,6 +36,25 @@ export default {
 </script>
 <style scoped>
 .card-recipe {
-	border: 1px solid rgb(172, 172, 172);
+	border-radius: 4px;
+}
+.divider {
+	border: 1px solid rgb(228, 228, 228);
+	height: 1px;
+	width: 98%;
+}
+.card-recipe:hover {
+	    box-shadow: 0px 3px 6px -2px rgba(0,0,0,0.2), 0px 2px 7px 0px rgba(0,0,0,0.14), 0px 1px 10px 0px rgba(0,0,0,0.12);
+			cursor: pointer;
+}
+.btn-icon {
+	text-align: right;
+	z-index: 10;
+}
+.btn-edit {
+	z-index: 10;
+}
+.btn-delete {
+	z-index: 11;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,117 @@
 <template>
-  <v-layout row wrap>
-    <v-flex xs12>
-      <v-text-field
-        prepend-inner-icon="search"
-        label="Buscar receita"
-        solo
-        :loading="true"
-      />
-    </v-flex>
-    <v-flex>
-      <recipe-card
-        title="TESTE"
-        content="OVO MEXIDOS COM SUCO DE LEITE E MARACUJA"
-      />
-    </v-flex>
-  </v-layout>
+  <v-container>
+    <v-layout row justify-center>
+      <v-flex md12>
+          <v-text-field
+            class="mt-1"
+            prepend-inner-icon="search"
+            label="Buscar receita"
+            solo
+            @input="search($event)"
+            :loading="!loaded"
+          />
+      </v-flex>
+    </v-layout>
+    <v-layout row justify-space-between>
+      <v-flex class="title mb-3 mt-4">
+        {{notFound ? 'Nenhuma receita encontrada' : 'SUAS RECEITAS' }}
+        <modal-new @create="createRecipe($event)"/>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap>
+      <v-flex>
+        <recipe-card
+          v-for="(recipe, index) in recipes"
+          :key="index"
+          class="my-2"
+          :title="recipe.title"
+          :content="recipe.content"
+          @click="viewRecipe(recipe)"
+          @delete="deleteRecipe(recipe)"
+          @edit="editModal(recipe)"
+        />
+      </v-flex>
+    </v-layout>
+    <!-- <modal-view :show="showModalView" :recipe="recipeToView" /> -->
+    <modal-edit :show="showModalEdit" :recipe="recipeToEdit" @edit="editRecipe($event)"/>
+  </v-container>
 </template>
 
 <script>
 import RecipeCard from '@/components/card/RecipeCard.vue'
+import ModalView from '@/components/modal/ViewRecipe.vue'
+import ModalNew from '@/components/modal/NewRecipe.vue'
+import ModalEdit from '@/components/modal/EditRecipe.vue'
+
+
 export default {
   components: {
-    RecipeCard
+    RecipeCard,
+    ModalView,
+    ModalNew,
+    ModalEdit
+  },
+  data() {
+    return {
+      recipes: [],
+      loaded: false,
+      notFound: false,
+      showModalView: false,
+      showModalEdit: false,
+      recipeToView: {},
+      recipeToEdit: {},
+      confirmCreate: null
+    }
+  },
+  created() {
+    this.load()
+  },
+  methods: {
+    async load()  {
+      await this.$axios.$get('http://localhost:8080/api/v0/recipe')
+      .then(res => {
+        this.loaded = true
+        this.recipes = res
+      })
+      .catch(err => console.err(err))
+    },
+    async createRecipe(recipe) {
+      await this.$axios.$post('http://localhost:8080/api/v0/recipe', recipe)
+      this.load()
+    },
+    async deleteRecipe(recipe) {
+      await this.$axios.$delete('http://localhost:8080/api/v0/recipe/' + recipe.id)
+      this.load()
+    },
+    async editRecipe(recipe) {
+      await this.$axios.$put('http://localhost:8080/api/v0/recipe/' + recipe.id, recipe)
+      this.load()
+    },
+    viewRecipe(recipe) {
+      this.recipeToView = recipe
+      this.showModalView = true
+    },
+    editModal(recipe) {
+      this.showModalEdit = true
+      this.recipeToEdit = recipe
+    },
+    async search(value) {
+      this.loaded = false
+      await this.load()
+      this.recipes =  this.recipes.filter(recipe => {
+        if(recipe.title.indexOf(value.toLowerCase()) >= 0) {
+          return recipe
+        }
+
+       } )
+      if(this.recipes.length === 0) {
+        this.notFound = true
+      } else {
+        this.notFound = false
+      }
+    }
   }
 }
 </script>
+<style scoped>
+</style>
